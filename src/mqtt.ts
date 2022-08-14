540b3a7d628c4eef9feb5f84e546028f5f05673b
Otso Jousimaa <otso@ojousima.net>
2022-08-14 10:11:08 +0300

    Read only Ruuvi Topics

--- src/mqtt.ts
+++ src/mqtt.ts
@@ -9,7 +9,7 @@ const client: mqtt.Client = mqtt.connect(`mqtt://${BROKER}`, opts);
 
 export const mqttInit = function (): void {
   client.on('connect', function () {
-    client.subscribe('#', function (err) {
+    client.subscribe('ruuvi/#', function (err) {
       if (!err) {
         console.log('MQTT Sub ok');
       } else {
@@ -21,18 +21,23 @@ export const mqttInit = function (): void {
   client.on('message', function (topic, message) {
     // message is Buffer
     // console.log(message.toString());
-    const post = JSON.parse(message.toString());
-    if (!post.data) {
-      console.log('invalid');
+    try {
+      const post = JSON.parse(message.toString());
+      if (!post.data) {
+        console.error('Not Ruuvi data');
+        return;
+      }
+      const ms = post.ts * 1000; // seconds, convert to ns for influx
+
+      const data = post.data.toUpperCase();
+      const tag_mac = topic.substring(topic.lastIndexOf('/') + 1);
+
+      const sample: RuuviData = new RuuviData(post.coords, tag_mac, post.gw_mac, 'Gateway', data, post.rssi, ms);
+
+      rDataToInflux(sample);
+    } catch (e) {
+      console.error('Not JSON data');
       return;
     }
-    const ms = post.ts * 1000; // seconds, convert to ns for influx
-
-    const data = post.data.toUpperCase();
-    const tag_mac = topic.substring(topic.lastIndexOf('/') + 1);
-
-    const sample: RuuviData = new RuuviData(post.coords, tag_mac, post.gw_mac, 'Gateway', data, post.rssi, ms);
-
-    rDataToInflux(sample);
   });
 };
